61390858432f2e61f416ee268511791ee21c4aac
skatta3 <56640542+skatta3@users.noreply.github.com>
2022-12-09 15:19:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello from Cyber Ark</h1>
       </header>
     </div>
   );
